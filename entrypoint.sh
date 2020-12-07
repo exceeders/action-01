b6c8a8642f18015aaff8a40fe64cace341a5e72f
John Son <40287191+son7211@users.noreply.github.com>
2020-12-07 16:57:38 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "sudo apt-get install ssh"
-echo "sudo apt-get install openssh-server"
+echo "apt-get install ssh"
+echo "apt-get install openssh-server"
 echo -e "\n\n\n" | ssh-keygen -t rsa 
